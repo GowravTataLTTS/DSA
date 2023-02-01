25c84bb9a3f797c10f255e04b058b878c7859d68
GowravTataLTTS <113660597+GowravTataLTTS@users.noreply.github.com>
2023-02-01 03:30:49 +0000

    Initial Commit

--- list/find_element_in_the_array.py
+++ list/find_element_in_the_array.py
@@ -3,4 +3,9 @@
 # given arr is the array , n is the dize of the array, idx is the index number
 
 def getByIndex(arr, n, idx):
-    return arr[idx] if id <= len(arr) else -1
+    return arr[idx] if idx <= n else -1
+
+
+arr = list(range(1,5))
+
+print(getByIndex(arr=arr,n=len(arr),idx=3))
